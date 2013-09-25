b5abc8d216a9f5e5066ece8a07140bb6dddc540e
rongmic <rongmic@gmail.com>
2013-09-25 10:44:30 +0800

    add category for groups

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20130924022901) do
+ActiveRecord::Schema.define(version: 20130925024119) do
+
+  create_table "categories", force: true do |t|
+    t.string   "name"
+    t.integer  "group_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "comments", force: true do |t|
     t.text     "content"
